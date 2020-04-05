1fb7ff789f10e3732c98eb570ff60d02f4d4c582
nha1012 <46342451+nha1012@users.noreply.github.com>
2020-04-05 18:22:41 +0700

    deploy

--- server.js
+++ server.js
@@ -47,9 +47,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 if ((process.env.NODE_ENV = 'production')) {
-    // app.use(cors({ origin: `http://localhost:3000` }));
     app.use(express.static('client/build'));
-
     app.get("*",(req,res)=>{
       res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
