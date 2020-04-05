73135783f49bb50bb08b1bbefef42b41f06429d2
nha1012 <46342451+nha1012@users.noreply.github.com>
2020-04-05 13:08:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 import morgan  from'morgan';
 import cors from'cors';
+import path from 'path';
 import bodyParser from'body-parser';
 import mongoose  from'mongoose';
 import jsonwebtoken from 'jsonwebtoken';
@@ -47,7 +48,11 @@ app.use(bodyParser.json());
 // app.use(cors()); // allows all origins
 if ((process.env.NODE_ENV = 'production')) {
     // app.use(cors({ origin: `http://localhost:3000` }));
-    app.use(express.static('client/build'))
+    app.use(express.static('client/build'));
+
+    app.get("*",(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
 }
 initSocket(io)
 // middleware
