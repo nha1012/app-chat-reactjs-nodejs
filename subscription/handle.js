15b88a3e43e9a6016bfbdfeeccca39bd7b4ea49f
Nha Nguyen <nguyenphucnha111@gmail.com>
2022-06-27 20:49:24 +0700

    done

--- subscription/handle.js
+++ subscription/handle.js
@@ -1,47 +1,65 @@
-const subscriptions = {};
-var crypto = require('crypto');
-const webpush = require('web-push');
+const subscriptions = {}
+var crypto = require("crypto")
+const webpush = require("web-push")
 
 const vapidKeys = {
-  privateKey: 'bdSiNzUhUP6piAxLH-tW88zfBlWWveIx0dAsDO66aVU',
+  privateKey: "bdSiNzUhUP6piAxLH-tW88zfBlWWveIx0dAsDO66aVU",
   publicKey:
-    'BIN2Jc5Vmkmy-S3AUrcMlpKxJpLeVRAfu9WBqUbJ70SJOCWGCGXKY-Xzyh7HDr6KbRDGYHjqZ06OcS3BjD7uAm8'
-};
+    "BIN2Jc5Vmkmy-S3AUrcMlpKxJpLeVRAfu9WBqUbJ70SJOCWGCGXKY-Xzyh7HDr6KbRDGYHjqZ06OcS3BjD7uAm8",
+}
 
-webpush.setVapidDetails('mailto:http://localhost:56966', vapidKeys.publicKey, vapidKeys.privateKey);
+webpush.setVapidDetails(
+  "mailto:example@yourdomain.org",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+)
 
 function createHash(input) {
-  const md5sum = crypto.createHash('md5');
-  md5sum.update(Buffer.from(input));
-  return md5sum.digest('hex');
+  const md5sum = crypto.createHash("md5")
+  md5sum.update(Buffer.from(input))
+  return md5sum.digest("hex")
 }
 
 function handlePushNotificationSubscription(req, res) {
-  const subscriptionRequest = req.body.data;
-  const susbscriptionId = createHash(JSON.stringify(subscriptionRequest));
-  subscriptions[susbscriptionId] = subscriptionRequest;
-  res.status(201).json({ id: susbscriptionId });
+  const subscriptionRequest = req.body.data
+  const susbscriptionId = createHash(JSON.stringify(subscriptionRequest))
+  subscriptions[susbscriptionId] = subscriptionRequest
+  res.status(201).json({ id: susbscriptionId })
 }
 
 function sendPushNotification(req, res) {
-  const subscriptionId = req.params.id;
-  const pushSubscription = subscriptions[subscriptionId];
+  const subscriptionId = req.params.id
+  const pushSubscription = subscriptions[subscriptionId]
   webpush
     .sendNotification(
       pushSubscription,
       JSON.stringify({
-        title: 'New Product Available ',
-        text: 'HEY! Take a look at this brand new t-shirt!',
-        image: '/images/jason-leung-HM6TMmevbZQ-unsplash.jpg',
-        tag: 'new-product',
-        url: '/new-product-jason-leung-HM6TMmevbZQ-unsplash.html'
+        title: "New Product Available ",
+        text: "HEY! Take a look at this brand new t-shirt!",
+        image: "/images/jason-leung-HM6TMmevbZQ-unsplash.jpg",
+        tag: "new-product",
+        url: "/new-product-jason-leung-HM6TMmevbZQ-unsplash.html",
       })
     )
-    .catch((err) => {
-      console.log(err);
-    });
+    .then(() => {
+      setTimeout(() => {
+        webpush.sendNotification(
+          pushSubscription,
+          JSON.stringify({
+            title: "New Product Available 2",
+            text: "HEY! Take a look at this brand new t-shirt!",
+            image: "/images/jason-leung-HM6TMmevbZQ-unsplash.jpg",
+            tag: "new-product",
+            url: "/new-product-jason-leung-HM6TMmevbZQ-unsplash.html",
+          })
+        )
+      }, 10000)
+    })
+    .catch(err => {
+      console.log(err)
+    })
 
-  res.status(202).json({});
+  res.status(202).json({})
 }
 
-module.exports = { handlePushNotificationSubscription, sendPushNotification };
+module.exports = { handlePushNotificationSubscription, sendPushNotification }
